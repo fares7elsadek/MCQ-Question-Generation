a10516909efef7ae90f5bb882477921fd5f82de1
fares elsadek <2021170383@cis.asu.edu.eg>
2025-01-01 04:28:01 +0200

    first commit

---
+++ app/models/question.py
@@ -0,0 +1,7 @@
+
+class Question():
+    def __init__(self,context , answer , distractors):
+        self.context = context
+        self.answer = answer
+        self.distractors = distractors
+        
